62bd17cf3c1013d44b24d1d6ceec1f0e732a7420
Hussam Alzahrani <halzahrani@oucs1444.otago.ac.nz>
2018-08-04 12:34:35 +1200

    add four opertions methods to the fraction class and printstatment in the main class

--- prog2.1/prog2.1/main.swift
+++ prog2.1/prog2.1/main.swift
@@ -13,15 +13,42 @@ var u: Fraction
 
 // Initialise the object
 u = Fraction(num: 2,den: 3)
+u.den = 0
 
 // Show the state of the object's internal variables
-print("u=\(u.num)/\(u.den)")
+print("u=\(u.description)")
+print("decimal value of u=\(u.decimal)")
 
 // Define and create a new instance of a Fraction object
 var x: Fraction = Fraction(num: -5)
 
-print("x=\(x.num)/\(x.den)")
+print("x=\(x.description)")
+print("decimal value of x=\(x.decimal)")
 
 
+var v = Fraction(num: -7, den: 9)
+
+print("v=\(v.description)")
+print("decimal value of v=\(v.decimal)")
+
+var f: Fraction
+
+f = u+v
+print("(" + u.description + ")+(" + v.description + ")=" + f.description)
+
+f = v-u
+print("(" + u.description + ")-(" + v.description + ")=" + f.description)
+
+f = Fraction.multiply(u , by: v )
+print("(" + u.description + ")*(" + v.description + ")=" + f.description)
+
+f = u*v
+print("(" + u.description + ")/(" + v.description + ")=" + f.description)
 
 
+f = u/v
+print("(" + u.description + ")+3=" + f.description)
+
+let z: Int = 2
+f = u.divide(z)
+print("(" + u.description + ")/\(z)=" + f.description)
